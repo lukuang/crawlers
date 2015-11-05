e3697e3dfec6318a44cd8ae44c147d6189c37798
lukuang <lukuang1989@gmail.com>
2015-11-05 14:28:55 -0500

    add delay between requests and use alert to stop program from running when error occur

--- news/Google/extension/crawler.js
+++ news/Google/extension/crawler.js
@@ -12,40 +12,105 @@ function my_function() {
     query = "q="+encodeURIComponent(query.replace(/ /,"+") )
     start_date = oForm.elements["start_date"]
     end_date = oForm.elements["end_date"]
+    url_prefix = url+query+"&"+document_type
+    page_id = 0;
+
     
+    request_url= compose_url(start_date,url_prefix,page_id)
+    file_name = start_date.value+"-"+page_id
 
-    //start_date = parse_date(oForm.elements["start_date"].value);
-    //end_date = parse_date(oForm.elements["end_date"].value)
-    while(start_date.value<=end_date.value){
-      date_string = parse_date(start_date.value)
-      date_range = "tbs="+encodeURIComponent("cdr:1,cd_min:"+date_string+",cd_max:"+date_string)
-      
-      original_request_url= url+query+"&"+document_type+"&"+date_range
-
-      for(i=0;i<=990;i+=10){
-
-        request_url = original_request_url+"&"+"start="+i;
-
-        //document.write(request_url);
-        
-        file_name = start_date.value+"-"+i
-        get_result_page(request_url, dest_url,file_name,post_data);
-          
-        
-        //document.write("<br>");
-        
-      }
-      start_date.stepUp(1);
-      //break;
-
-    }
+    //get_result_page(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper);
+    get_result_page(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper);
 }
 
-function post_data(url,file_name,responseText){
+function compose_url(start_date,url_prefix,page_id){
+  date_string = parse_date(start_date.value)
+
+  date_range = "tbs="+encodeURIComponent("cdr:1,cd_min:"+date_string+",cd_max:"+date_string)
+      
+  request_url= url_prefix+"&"+date_range+"&start="+page_id
+  return request_url;
+}
+
+function delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,milliseconds){
+  //setTimeout(function(){get_test(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper);},milliseconds);
+  setTimeout(function(){get_result_page(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper);},milliseconds);
+  
+  //document.write(asdasdasd);
+  
+}
+
+function call_back_wrapper(start_date,end_date,url_prefix,page_id,dest_url,file_name,responseText){
+  //sleep(10000);
+  
+  post_data(dest_url,file_name,responseText)
+  if(page_id!=990){
+    page_id+=10;
+    request_url= url_prefix+"&"+date_range+"&start="+page_id
+    file_name = start_date.value+"-"+page_id
+    delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,10000);
+  }
+  else if(start_date.value<=end_date.value){
+    start_date.stepUp(1);
+    page_id = 0;
+    request_url= url_prefix+"&"+date_range+"&start="+page_id
+    file_name = start_date.value+"-"+page_id
+    delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,10000);    
+  }
+  else{
+    document.write("Finished!")
+  }
+}
+
+
+function get_test(start_date,end_date,url_prefix,page_id,url,dest_url,file_name,cb){
+  document.write("getting "+url);
+  document.write("</br>");
+  if( typeof cb === 'function' )
+        cb(start_date,end_date,url_prefix,page_id,dest_url,file_name,"xx")
+}
+
+function post_test(dest_url,file_name,responseText){
+  document.write("posting "+dest_url);
+  document.write("</br>");
+}
+
+
+function get_result_page(start_date,end_date,url_prefix,page_id,url,dest_url,file_name,cb){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // innerText does not let the attacker inject HTML elements.
+      //document.getElementById("resp").innerText = xhr.responseText;
+      //console.log(xhr.responseText)
+      if( typeof cb === 'function' )
+        cb(start_date,end_date,url_prefix,page_id,dest_url,file_name,xhr.responseText)
+    }
+    else if(xhr.readyState == 4 && xhr.status != 200){
+      document.write("error in getting "+url);
+      document.write("</br>");
+      document.write(xhr.statusText);
+      document.write("</br>");
+      console.log("error in getting to "+url);
+      console.log(xhr.statusText);
+      console.log("current file is "+file_name)
+      alert("STOPPED!")
+      throw "";
+
+    }
+
+  }
+  xhr.send();
+}
+
+
+
+function post_data(dest_url,file_name,responseText){
   //console.log(responseText)
   var xhr = new XMLHttpRequest();
   var params = "f="+file_name+"&d="+encodeURIComponent(responseText);
-  xhr.open("post", url, true);
+  xhr.open("post", dest_url, true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   //xhr.setRequestHeader("Content-length", params.length);
   //xhr.setRequestHeader("Connection", "close");
@@ -55,12 +120,16 @@ function post_data(url,file_name,responseText){
       //document.getElementById("resp").innerText = xhr.responseText;
       //console.log(xhr.responseText)
     }
-    else{
-      document.write("error in posting to "+url);
+    else if(xhr.readyState == 4 &&xhr.status != 200){
+      document.write("error in posting to "+dest_url);
       document.write("</br>");
       document.write(xhr.statusText);
       document.write("</br>");
-      throw new Error("Something went badly wrong!");
+      console.log("error in posting to "+dest_url);
+      console.log(xhr.statusText);
+      console.log("current file is "+file_name)
+      alert("STOPPED!")
+      throw "";
     }
 
   }
@@ -73,27 +142,4 @@ function parse_date(date_string){
   return result
 }
 
-function get_result_page(url,dest_url,file_name,cb){
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // innerText does not let the attacker inject HTML elements.
-      //document.getElementById("resp").innerText = xhr.responseText;
-      //console.log(xhr.responseText)
-      if( typeof cb === 'function' )
-        cb(dest_url,file_name,xhr.responseText)
-    }
-    else{
-      document.write("error in getting "+url);
-      document.write("</br>");
-      document.write(xhr.statusText);
-      document.write("</br>");
-      throw "";
-    }
-
-  }
-  xhr.send();
-  setTimeout(10000);
-}
 
