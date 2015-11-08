5d154f0f0c6bd9417e71ddf407676d541cf4d6ae
lukuang <lukuang1989@gmail.com>
2015-11-08 15:18:20 -0500

    final version for the crawler extension

--- news/Google/extension/crawler.js
+++ news/Google/extension/crawler.js
@@ -1,4 +1,6 @@
-
+var origin_wait = 5;
+var LIMIT = 3600;
+var PREVIOUS_SUCCESS = true;
 document.getElementById("set_query").addEventListener("click",my_function);
 function my_function() {
 
@@ -13,14 +15,15 @@ function my_function() {
     start_date = oForm.elements["start_date"]
     end_date = oForm.elements["end_date"]
     url_prefix = url+query+"&"+document_type
-    page_id = 0;
+    page_id =  parseInt(oForm.elements["starting_pageid"].value,10);
 
     
     request_url= compose_url(start_date,url_prefix,page_id)
     file_name = start_date.value+"-"+page_id
+    var now_wait = origin_wait;
 
     //get_result_page(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper);
-    get_result_page(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper);
+    get_result_page(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name, now_wait,call_back_wrapper);
 }
 
 function compose_url(start_date,url_prefix,page_id){
@@ -32,30 +35,50 @@ function compose_url(start_date,url_prefix,page_id){
   return request_url;
 }
 
-function delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,milliseconds){
+function delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,seconds){
   //setTimeout(function(){get_test(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper);},milliseconds);
-  setTimeout(function(){get_result_page(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper);},milliseconds);
+  //console.log("wait for "+seconds)
+  setTimeout(function(){get_result_page(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,seconds,call_back_wrapper);},seconds*1000);
   
   //document.write(asdasdasd);
   
 }
 
-function call_back_wrapper(start_date,end_date,url_prefix,page_id,dest_url,file_name,responseText){
+function has_next(responseText){
+  var doc = document.implementation.createHTMLDocument( 'html' );
+  //console.log(responseText)
+  doc.documentElement.innerHTML = responseText
+  console.log()
+  id = "pnnext"
+  var has = doc.getElementById(id)
+  return(typeof(has)!=undefined && has!=null)
+
+}
+
+function call_back_wrapper(start_date,end_date,url_prefix,page_id,dest_url,file_name, now_wait,responseText){
   //sleep(10000);
+  if (PREVIOUS_SUCCESS==true && now_wait!=origin_wait){
+    now_wait = origin_wait;
+    document.write("reset timer to "+now_wait);
+    document.write("</br>");
+  }
   
-  post_data(dest_url,file_name,responseText)
+  if( !has_next(responseText) ){
+    page_id = 990
+  }
+
   if(page_id!=990){
     page_id+=10;
-    request_url= url_prefix+"&"+date_range+"&start="+page_id
+    request_url= compose_url(start_date,url_prefix,page_id)
     file_name = start_date.value+"-"+page_id
-    delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,10000);
+    delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,now_wait);
   }
   else if(start_date.value<=end_date.value){
     start_date.stepUp(1);
     page_id = 0;
-    request_url= url_prefix+"&"+date_range+"&start="+page_id
+    request_url= compose_url(start_date,url_prefix,page_id)
     file_name = start_date.value+"-"+page_id
-    delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,10000);    
+    delay_get(start_date,end_date,url_prefix,page_id,request_url, dest_url,file_name,call_back_wrapper,now_wait);    
   }
   else{
     document.write("Finished!")
@@ -76,27 +99,43 @@ function post_test(dest_url,file_name,responseText){
 }
 
 
-function get_result_page(start_date,end_date,url_prefix,page_id,url,dest_url,file_name,cb){
+function get_result_page(start_date,end_date,url_prefix,page_id,url,dest_url,file_name, now_wait,cb){
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
+  console.log(url)
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
+      PREVIOUS_SUCCESS = true;
+      post_data(dest_url,file_name,xhr.responseText)
       // innerText does not let the attacker inject HTML elements.
       //document.getElementById("resp").innerText = xhr.responseText;
       //console.log(xhr.responseText)
       if( typeof cb === 'function' )
-        cb(start_date,end_date,url_prefix,page_id,dest_url,file_name,xhr.responseText)
+        cb(start_date,end_date,url_prefix,page_id,dest_url,file_name, now_wait, xhr.responseText)
     }
     else if(xhr.readyState == 4 && xhr.status != 200){
-      document.write("error in getting "+url);
+      PREVIOUS_SUCCESS = false;
+      document.write("error in getting "+file_name);
+      console.log("error in getting to "+url);
+      document.write("</br>");
+      document.write(Date())
       document.write("</br>");
       document.write(xhr.statusText);
       document.write("</br>");
-      console.log("error in getting to "+url);
-      console.log(xhr.statusText);
-      console.log("current file is "+file_name)
-      alert("STOPPED!")
-      throw "";
+      if(now_wait>=LIMIT){
+        document.write(now_wait+" exceed maximum timeout!");
+        alert("STOPPED!")
+      }
+      else{
+        now_wait *= 2;
+        document.write("restart for wait "+now_wait);
+        document.write("</br>");
+        delay_get(start_date,end_date,url_prefix,page_id,url, dest_url,file_name,call_back_wrapper,now_wait);
+        //get_result_page(start_date,end_date,url_prefix,page_id,url,dest_url,file_name, now_wait,cb);
+      }
+      
+      //alert("STOPPED!")
+      //throw "";
 
     }
 
